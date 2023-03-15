d15cedb21d02b921538ef8c51bea2cb8e421db00
Jiaying Li <jiaying.li0803@gmail.com>
2023-03-15 13:16:04 -0400

    Delete Assignments/InClassExercise/quantization directory

--- Assignments/InClassExercise/quantization/quantizeAudio.m
+++
@@ -1,9 +0,0 @@
-function yQuant = quantizeAudio(y, bitDepth)
-
-yQuant = zeros(length(y), 1); % initialize yQuant
-
-for i=1:length(y) % apply quantization to each point in the input signal
-    yQuant(i) = quantizeMidTread(y(i), bitDepth); % quantization
-end
-
-end
