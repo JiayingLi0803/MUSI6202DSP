9b0b2c463770424c1df11280d45dee4d8e42a9b0
Jiaying Li <jiaying.li0803@gmail.com>
2023-04-17 12:05:32 -0400

    Add files via upload

---
+++ Assignments/Assignments/Assignment2/group02-feedback/generateBlocks.m
@@ -0,0 +1,18 @@
+function [t, x_blk] = generateBlocks(x, sample_rate_hz, block_size, hop_size)
+
+% block_num = ceil((length(x)-(block_size-hop_size))/hop_size); % find block number
+%%! KW: wrong block num
+block_num = ceil((length(x)-block_size)/hop_size) + 1;
+%%! /KW
+% x = [x; zeros(mod(length(x)-block_size,hop_size), 1)]; % zero pad x for the last block
+%%! KW: wrong padding
+x = [x; zeros((block_num-1) * hop_size + block_size - length(x), 1)];
+
+
+x_blk = zeros(block_size,block_num); % initialize x_blk matrix
+t = zeros(block_num,1); % initialize t
+for i = 1:block_num
+    x_blk(:, i) = x(hop_size*(i-1)+1:hop_size*(i-1)+block_size); % copy blocks of x into x_blk matrix
+    t(i) = hop_size*(i-1)/sample_rate_hz; % use time stamp
+end
+end
